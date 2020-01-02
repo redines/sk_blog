9550b0967fd993274660e8f5bfb0ef53755094f1
Redines <pontus.g91@gmail.com>
2020-01-02 17:14:48 +0100

    fixed connecting to db, finished reading from db

--- api/objects/posts.php
+++ api/objects/posts.php
@@ -1,19 +1,31 @@
 <?php
-class Product{
- 
-    // database connection and table name
-    private $conn;
-    private $table_name = "products";
- 
-    // object properties
-    public $id;
-    public $date;
-    public $text;
+    class Posts{
+        // database connection and table name
+        private $conn;
+        private $table_name = "posts";
+    
+        // object properties
+        public $id;
+        public $date;
+        public $text;
+    
+        // constructor with $db as database connection
+        public function __construct($db){
+            $this->conn = $db;
+        }
 
- 
-    // constructor with $db as database connection
-    public function __construct($db){
-        $this->conn = $db;
+        // read products
+        function read(){
+        
+            // select all query
+            $query = "SELECT * FROM posts";
+            // prepare query statement
+            $stmt = $this->conn->prepare($query);
+        
+            // execute query
+            $stmt->execute();
+        
+            return $stmt;
+        }
     }
-}
 ?>
