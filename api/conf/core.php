d04c37fe46e74515eedda13c7f1da5d0c5a8d693
Redines <pontus.g91@gmail.com>
2020-01-08 11:42:06 +0100

    added jwt lib, add login with jwt

---
+++ api/conf/core.php
@@ -0,0 +1,14 @@
+<?php
+// show error reporting
+error_reporting(E_ALL);
+ 
+// set your default time-zone
+date_default_timezone_set('Europe/Stockholm');
+ 
+// variables used for jwt
+$key = "testing";
+$iss = "http://skblog.se";
+$aud = "http://skblog.se";
+$iat = 1356999524;
+$nbf = 1357000000;
+?>
