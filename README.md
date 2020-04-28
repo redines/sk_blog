3ea0a0b8ab4b71231f79bb22f98ecba83c9209b1
Redines <pontus.g91@gmail.com>
2020-04-28 21:02:25 +0200

    added gitignore to prevent pushing node packages, added npm to project, added expressj to project, added simple first expressjs file

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # sk_blog
 A blog website I made from scratch for use during my study trip to S.korea
 
-cred goes to this guide for helping me learn a way to creat API in PHP:
-https://www.codeofaninja.com/2017/02/create-simple-rest-api-in-php.html
---
+++ api/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
