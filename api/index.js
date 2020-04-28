eaa978d871a01ee9523eeac1838ef9228105404f
Redines <pontus.g91@gmail.com>
2020-04-28 21:38:18 +0200

    added mariadb database connection

--- api/index.js
+++ api/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const mariadb = require('mariadb');
+
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
@@ -12,8 +15,33 @@ app.use(function(req, res, next) {
   next();
 });
 
+
+const pool = mariadb.createPool({
+     host: 'localhost', 
+     user:'root', 
+     password: '',
+     database: 'skblog',
+     connectionLimit: 5
+});
+
+pool.getConnection()
+.then(conn => {
+    conn.query("SELECT * from posts")
+      .then((rows) => {
+        console.log(rows);
+
+      })
+      .catch(err => {
+        //handle error
+        console.log(err); 
+        conn.end();
+      })
+      
+  }).catch(err => {
+    //not connected
+  });
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
-const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
